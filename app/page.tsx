fd601f1157bd89180e9ac5580402267bd55cc42f
Jaydex-69 <jawadtariq600@gmail.com>
2026-04-22 05:04:44 -0700

    added navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="text-3xl text-indigo-500">CoinPulse</div>
+    <div></div>
   )
 }
 
---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+
+const Header = () => {
+  const pathname = usePathname()
+  return (
+    <header>
+      <div className='main-container inner'>
+       <Link href="/"> 
+           <img src="/logo.svg" alt="logo" width={132} height={40} />
+       </Link>
+       <nav>
+           <Link href='/' className={cn('nav-link', {
+            'is-active': pathname === '/',
+            'is-home': true
+           })}>
+            Home
+           </Link>
+           <p>
+            Search Modal
+           </p>
+           <Link href='/coins' className={cn('nav-link', {
+            'is-active': pathname === '/coins'
+           })}>
+               All coins
+           </Link>
+        </nav>
+       </div>
+    </header>
+  )
+}
+
+export default Header
